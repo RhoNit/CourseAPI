9c59f589b81a2ccdddc1524b9e5dd36cf5f61ce5
RhoNit <tb.edumax29@gmail.com>
2022-11-11 10:23:31 +0530

    data.py is added

---
+++ apis/data.py
@@ -0,0 +1,36 @@
+courses = {
+    1: {
+        "title": "Physics 101",
+        "teacher": "Dr. John Morrison",
+        "students": ["Sushant Shahi", "Mayank Raj Singh"],
+        "level": "Intermediate"
+    },
+
+    2: {
+        "title": "Advanced Data Structures",
+        "teacher": "Prof. Shashank Bholane",
+        "students": ["Ranit Biswas", "Ritik Sharma", "Niraj Lohani"],
+        "level": "Advanced"
+    },
+
+    3: {
+        "title": "Fundamentals of Cloud Computing",
+        "teacher": "Prof. Nalini Mehrotra",
+        "students": ["Utkarsh", "Ranit Biswas", "Sushant Shahi", "Ayush Jha"],
+        "level": "Beginner"
+    },
+
+    4: {
+        "title": "Big Data",
+        "teacher": "Timothy Jacobs",
+        "students": ["Mayank Raj Singh", "Ritik Sharma", "Niraj Lohani"],
+        "level": "Beginner"
+    },
+
+    5: {
+        "title": "API Development & Testing in Python",
+        "teacher": "Stuart Richardson",
+        "students": ["Niraj Lohani", "Sushant Shahi", "Ranit Biswas", "Utkarsh", "Ritik Sharma"],
+        "level": "Intermediate"
+    }
+}
